fb223de22200b9914689a7426e52b2c0051425b2
nagnos <hasong@ucsd.edu>
2019-12-08 12:23:33 -0800

    Display static page

--- insdjango/urls.py
+++ insdjango/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from insdjango.views import HelloWorld
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('insta/', include('insdjango.urls')),
+    path('', HelloWorld.as_view(), name='helloworld')
 ]
